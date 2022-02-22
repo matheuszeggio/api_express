94e15f89355e4fceef8f2e67ba8356774145230d
matheuszeggio <matheus@smartinnovation.com.br>
2022-02-22 20:10:08 -0300

    definindo as validacoes com a lib celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 import routes from './routes';
 import AppError from '../errors/AppError';
+import { errors } from 'celebrate';
 import '../typeorm';
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
     (
